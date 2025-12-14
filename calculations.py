b2bb9a385237399f3b5e7ee8233111f4d0c19719
maphy9 <maphy9@gmail.com>
2025-12-14 13:56:46 +0100

    Add distance calculations

--- calculations.py
+++ calculations.py
@@ -31,6 +31,13 @@ def get_coords(landmarks, idx):
     lm = landmarks.landmark[idx]
     return (lm.x, lm.y, lm.z)
 
+def get_coords_2d(landmarks, idx):
+    lm = landmarks.landmark[idx]
+    return (lm.x, lm.y)
+
+def calculate_distance_2d(p1, p2):
+    return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
 def calculate_front_angles(pose_landmarks):
     angles = {}
     
